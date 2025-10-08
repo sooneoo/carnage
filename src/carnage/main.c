58f7db7310c0e1f4109a9caa796315320af8fb80
Petr Horáček <phoracek.edit@gmail.com>
2025-10-08 11:50:20 +0200

    0.1.0 - version system

--- src/carnage/main.c
+++ src/carnage/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "version.h"
 
 int main(void) {
+    Carnage_Version version = carnage_version();
+    printf("current version: %d.%d.%d\n", version.major, version.minor, version.patch);
     printf("program exit..\n");
     return EXIT_SUCCESS;
 }
