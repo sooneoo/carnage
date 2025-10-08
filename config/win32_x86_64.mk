3dbaf712a442d27b3a55377c93305781af3f1057
Petr Horáček <phoracek.edit@gmail.com>
2025-10-08 11:35:18 +0200

    base project structure

---
+++ config/win32_x86_64.mk
@@ -0,0 +1,27 @@
+CC = gcc
+
+PKGS = raylib
+
+
+CFLAGS += -Wall
+CFLAGS += -Wextra
+CFLAGS += -pedantic
+CFLAGS += -Isrc/include
+CFLAGS += $$(pkg-config --cflags $(PKGS))
+
+LIBS += -lm
+LIBS += -lalloc
+LIBS += $$(pkg-config --libs $(PKGS))
+
+
+OBJS += $(CACHE)/main.o
+
+
+$(CACHE)/main.o: src/carnage/main.c
+	$(CC) $(CFLAGS) -c src/carnage/main.c -o $@
+
+
+$(RELEASE)/$(TARGET): $(OBJS)
+	$(CC) $(OBJS) $(LIBS) -o $(RELEASE)/$(TARGET)
+
+
